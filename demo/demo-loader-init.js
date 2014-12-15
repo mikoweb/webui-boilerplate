64b6054c312e023ff342ec250a4cec6b1ef1702e
Rafał Mikołajun <rafal@mikoweb.pl>
2014-12-15 15:59:16 +0000

    drobne zmiany

--- demo/demo-loader-init.js
+++ demo/demo-loader-init.js
@@ -1,10 +1,17 @@
+jsloader.onLoad("framework", function () {
+    "use strict";
+    require(["jquery"], function ($) {
+        var body = $("body");
+        $.get("include/header.html", function(data) {body.prepend(data);});
+        $.get("include/footer.html", function(data) {body.append(data);});
+    });
+});
+
 jsloader.onLoad(function () {
     "use strict";
-    require(['jquery.app', 'jquery.hash-router'], function(app, router) {
-        app.theme.ready(function () {
-            router.getReady();
-        });
+    require(['jquery.app'], function(app) {
         app.theme.init();
     });
 });
+
 jsloader.init();

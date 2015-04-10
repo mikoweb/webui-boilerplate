3860308da629b17b1148553445328d0acaa1dea7
Rafał Mikołajun <rafal@mikoweb.pl>
2015-04-10 12:03:49 +0000

    tinymce nie będzie aktualizowany przez bowera ze względu na dodatkowe pluginy i języki

--- webui/vendor/tinymce-dist/plugins/codemirror/langs/pl.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('pl',{
-	'HTML source code': 'Kod źródłowy',
-	'Start search': 'Znajdź',
-	'Find next': 'Znajdź następny',
-	'Find previous': 'Znajdź poprzedni',
-	'Replace': 'Zamień',
-	'Replace all': 'Zamień wszystko'
-});

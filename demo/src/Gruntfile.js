65af12071e6819a9af5ecbd6fd8a04711de1c16b
Rafał Mikołajun <rafal@mikoweb.pl>
2016-11-01 18:46:31 +0100

    better babel

--- demo/src/Gruntfile.js
+++ demo/src/Gruntfile.js
@@ -1,93 +1,17 @@
+var webuiBabel = require('webui-grunt-babel'),
+    util = require('util');
+
 module.exports = function(grunt) {
     "use strict";
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        path: {
-            src: 'js/',
-            dist: '../dist/'
-        },
-        babel: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= path.src %>',
-                    src: ['**/*.js'],
-                    dest: '<%= path.dist %>',
-                    ext: '.js'
-                }]
-            }
-        },
-        replace: {
-            dist: {
-                options: {
-                    patterns: [
-                        {
-                            match: /^define\('js\//g,
-                            replacement: "define('"
-                        }
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= path.dist %>',
-                        src: ['**/*.js'],
-                        dest: '<%= path.dist %>',
-                        ext: '.js'
-                    }
-                ]
-            }
-        },
-        uglify: {
-            dist: {
-                expand: true,
-                cwd: '<%= path.dist %>',
-                src: ['**/*.js'],
-                dest: '<%= path.dist %>',
-                ext: '.js'
-            },
-            babelHelpers: {
-                files: {
-                    '../babel-helpers.js': ['../babel-helpers.js']
-                }
-            }
-        },
-        watch: {
-            dist: {
-                files: ['<%= path.src %>**/*.js'],
-                tasks: ['babel', 'replace', 'uglify'],
-                options: {
-                    spawn: false,
-                    reload: true
-                }
-            },
-            devDist: {
-                files: ['<%= path.src %>**/*.js'],
-                tasks: ['babel', 'replace'],
-                options: {
-                    spawn: false,
-                    reload: true
-                }
-            }
-        }
-    });
+    grunt.initConfig(util._extend(webuiBabel.getConfig({
+        src: 'js',
+        dist: '../dist',
+        srcFolder: 'js',
+        babelHelpers: '../babel-helpers.js'
+    }), {
+        pkg: grunt.file.readJSON('package.json')
+    }));
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
-
-    grunt.registerTask('dist', [
-        'babel:dist',
-        'replace:dist',
-        'uglify:dist',
-        'uglify:babelHelpers'
-    ]);
-
-    grunt.registerTask('dev-dist', [
-        'babel:dist',
-        'replace:dist',
-        'uglify:babelHelpers'
-    ]);
+    webuiBabel.registerTasks(grunt);
 };
